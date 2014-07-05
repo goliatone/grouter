23c57d6170e20d66cb43b0df09eb2b1f2c1b316b
goliatone <burgosemi@gmail.com>
2014-07-05 11:18:51 -0400

    Initial commit

---
+++ examples/main.js
@@ -0,0 +1,14 @@
+/*global define:true requirejs:true*/
+/* jshint strict: false */
+requirejs.config({
+    paths: {
+        'jquery': 'jquery/jquery',
+        'grouter': 'grouter'
+    }
+});
+
+define(['grouter', 'jquery'], function (GRouter, $) {
+    console.log('Loading');
+	var grouter = new GRouter();
+	grouter.init();
+});
---
+++ test/spec/grouter-spec.js
@@ -0,0 +1,23 @@
+/*global define:true, describe:true , it:true , expect:true, 
+beforeEach:true, sinon:true, spyOn:true , expect:true */
+/* jshint strict: false */
+define(['grouter', 'jquery'], function(GRouter, $) {
+
+    describe('just checking', function() {
+
+        it('GRouter should be loaded', function() {
+            expect(GRouter).toBeTruthy();
+            var grouter = new GRouter();
+            expect(grouter).toBeTruthy();
+        });
+
+        it('GRouter should initialize', function() {
+            var grouter = new GRouter();
+            var output   = grouter.init();
+            var expected = 'This is just a stub!';
+            expect(output).toEqual(expected);
+        });
+        
+    });
+
+});
