4916a09df1e36549db91632d8f2fcc807b69188c
goliatone <burgosemi@gmail.com>
2014-07-06 01:07:34 -0400

    SPEC: WIPing

--- test/spec/grouter-spec.js
+++ test/spec/grouter-spec.js
@@ -3,21 +3,32 @@ beforeEach:true, sinon:true, spyOn:true , expect:true */
 /* jshint strict: false */
 define(['grouter'], function(GRouter) {
 
-    describe('just checking', function() {
+    describe('GRouter', function() {
+        var router;
+        beforeEach(function() {
+            router = new GRouter();
+        });
 
-        it('GRouter should be loaded', function() {
+
+        it('should be loaded', function() {
             expect(GRouter).toBeTruthy();
             var grouter = new GRouter();
             expect(grouter).toBeTruthy();
         });
 
-        it('GRouter should initialize', function() {
-            var grouter = new GRouter();
-            var output = grouter.init();
-            var expected = 'This is just a stub!';
-            expect(output).toEqual(expected);
+        it('should have a DEFAULTS class property', function() {
+            expect(GRouter.DEFAULTS).toBeTruthy();
         });
 
-    });
+        it('should have an initialRoute by default', function() {
+            GRouter.DEFAULTS.initialRoute = 'home';
+            expect(router.initialRoute).toEqual('home');
+        });
 
+        it('currentId if not given as an option in config object should match initialRoute', function() {
+            expect(router.currentId).toEqual(router.initialRoute);
+        });
+
+
+    });
 });
