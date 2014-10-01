d91a5213d94f766740d9035765f7b2eb5c3585b1
goliatone <burgosemi@gmail.com>
2014-09-30 23:17:31 -0400

    EXAMPLE: WIP matcher

--- examples/main.js
+++ examples/main.js
@@ -2,12 +2,33 @@
 /* jshint strict: false */
 requirejs.config({
     paths: {
-        'jquery': 'jquery/jquery',
-        'grouter': 'grouter'
+        'grouter': 'grouter',
+        'gpub':'gpub/gpub',
+        'extend':'gextend/extend'
     }
 });
 
-define(['grouter', 'jquery'], function (GRouter, $) {
+define(['grouter', 'gpub'], function (GRouter, GPub) {
     console.log('Loading');
+
+    GPub.observable(GRouter);
+
+
 	var grouter = new GRouter();
+
+	grouter.match('/home', function home(e){
+		console.log('HOME', e);
+	});
+
+	grouter.match('/product/23', function productDetail(e){
+		console.log('PRODUCT 23', e);
+	});
+
+	grouter.not('/home', function notHome(e){
+		console.log('NOT HOME');
+	});
+
+	grouter.matcher('/home', {userID:23});
+	grouter.matcher('/product/23', {userID:23});
+	grouter.matcher('/product/something', {userID:23});
 });
