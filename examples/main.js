603d8842781e4b7d87b9ce4eca90b9b7cbc53a2c
goliatone <burgosemi@gmail.com>
2014-09-30 23:37:39 -0400

    EXAMPLE: main.js WIP

--- examples/main.js
+++ examples/main.js
@@ -17,18 +17,27 @@ define(['grouter', 'gpub'], function (GRouter, GPub) {
 	var grouter = new GRouter();
 
 	grouter.match('/home', function home(e){
-		console.log('HOME', e);
+		console.debug('HOME', e);
 	});
 
-	grouter.match('/product/23', function productDetail(e){
-		console.log('PRODUCT 23', e);
+	grouter.match('/product/\\d+', function productDetail(e){
+		console.debug('PRODUCT 23', e);
+	});
+
+	grouter.match('/unhandled/example', function productDetail(e){
+		console.error('THIS SHOULD NOT FIRE', e);
 	});
 
 	grouter.not('/home', function notHome(e){
 		console.log('NOT HOME');
 	});
 
+	grouter.unhandled(function(e){
+		console.info('\nUNHANDLED', e.path);
+	});
+
 	grouter.matcher('/home', {userID:23});
 	grouter.matcher('/product/23', {userID:23});
+	grouter.matcher('/product/566', {userID:23});
 	grouter.matcher('/product/something', {userID:23});
 });
