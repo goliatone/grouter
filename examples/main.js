f123965c77fa74d785b043aea96d9043b6efdf0c
goliatone <burgosemi@gmail.com>
2014-10-04 22:12:44 -0400

    EXAMPLE: Updated to reflect API refactoring. Added pathtoregexp dependency

--- examples/main.js
+++ examples/main.js
@@ -4,48 +4,56 @@ requirejs.config({
     paths: {
         'grouter': 'grouter',
         'gpub':'gpub/gpub',
+        'pathtoregexp':'pathtoregexp/pathtoregexp',
         'extend':'gextend/extend'
     }
 });
 
-define(['grouter', 'gpub'], function (GRouter, GPub) {
+define(['grouter', 'gpub', 'pathtoregexp'], function (GRouter, GPub, pathtoregexp) {
     console.log('Loading');
 
     GPub.observable(GRouter);
 
 
-	var grouter = new GRouter();
-
-	grouter.match('home', function home(e){
-		console.debug('HOME', e);
+	var grouter = new GRouter({
+		pathToRegexp:pathtoregexp
 	});
 
-	grouter.match('product.:id(\\d+)', function productDetail(e){
-		console.debug('PRODUCT ', e.params.id);
+	grouter.route('home', function home(e){
+		console.log('\n=========================');
+		console.debug('HOME', e.path);
 	});
 
-	grouter.match('unhandled.example', function productDetail(e){
+	grouter.route('product/:id(\\d+)', function productDetail(e){
+		console.log('\n=========================');
+		console.debug('PRODUCT:', e.params.id);
+	});
+
+	grouter.route('unhandled/example', function productDetail(e){
+		console.log('\n=========================');
 		console.error('THIS SHOULD NOT FIRE', e);
 	});
 
-	grouter.match('*', function notHome(e){
-		console.warn('==> UNHANDLED', e.path);
+	grouter.on('all', function(target, e){
+		// console.log('HERE ALL IS', e.path, e.params);
 	});
+
+	grouter.route(/route\/president_(.*)/, function(e) {
+		console.log('\n=========================');
+        console.warn('MATCHING ANY ROUTE', e.path);
+    });
 
 	grouter.unhandled(function(e){
-		console.info('\nUNHANDLED', e.path);
+		console.info('===> UNHANDLED', e.path);
 	});
 
-	grouter.on('all', function(target, e){
-		console.log('HERE ALL IS', e.path, e.params)
-	});
+	grouter.match('home');
+	grouter.match('product/23', {userID:23});
+	grouter.match('product/566', {userID:23});
+	grouter.match('product/unhandled');
+	grouter.match('route/president_bill_sign');
+	grouter.match('route/president_veto');
+	grouter.match('route/president_veto_override_vote_start');
 
-	grouter.not('home', function notHome(e){
-		console.warn('NOT HOME', e.path);
-	});
-
-	grouter.matcher('home', {userID:23});
-	grouter.matcher('product.23', {userID:23});
-	grouter.matcher('product.566', {userID:23});
-	grouter.matcher('product.unhandled', {userID:23});
+	window.rt = grouter;
 });
